c097bac8d9f94b38ba434f3e5553ea34c11e5799
FaroKenshin <74579076+FaroKenshin@users.noreply.github.com>
2020-11-17 10:38:54 +0700

    Update index.js

--- index.js
+++ index.js
@@ -13,11 +13,9 @@ const tambahan = require("./lib/tambahan.js")
 const donasi = require("./lib/donasi.js");
 const info = require("./lib/info.js");
 /////////////////
-const BotName = 'REUZ BOT☄'; 
-const instagram = 'https://instagram.com/reuz.id'; 
-const telegram = 'https://t.me/reuzzz'; 
-const kapanbotaktif = 'KETIKA HANYA DIAKTIFKAN'; 
-const youtube = 'https://www.youtube.com/c/reuz_id';
+const BotName = 'FaroKenshin'; 
+const instagram = 'https://www.instagram.com/faro_kenshin/'; 
+const kapanbotaktif = 'Ketika Lagi Sibuk Mabar'; 
 //const grupch1 = 'belum ada grup'; 
 //const grupch2 = 'belum ada grup' ; 
 const
